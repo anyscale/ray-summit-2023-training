fb1193489d23a25e6177b245b8c68ef470ae5198
Disiok <simonsdsuo@gmail.com>
2023-09-16 00:16:57 -0700

    wip
    clear output
    
    wip
    
    wup
    
    remove key
    
    wip
    
    wip
    
    wip
    
    wip

--- Ray-LlamaIndex/notebooks/utils.py
+++ Ray-LlamaIndex/notebooks/utils.py
@@ -1,4 +1,6 @@
 import numpy as np
+import json
+import random
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.embeddings.huggingface import HuggingFaceEmbeddings
 
@@ -67,11 +69,71 @@ def get_retriever(
 ):
 
     embed_model = get_embedding_model(embedding_model_name)
-    service_context = ServiceContext.from_defaults(embed_model=embed_model)
+    service_context = ServiceContext.from_defaults(embed_model=embed_model, llm=None)
 
     index = _get_vector_store_index(service_context)
     return index.as_query_engine(similarity_top_k=similarity_top_k)
 
-    
 
-    
+def train_test_split(data, split_ratio=0.8):
+    """
+    Split a list of items into training and testing sets.
+
+    Args:
+        data (list): The list of items to be split.
+        split_ratio (float): The ratio of items to include in the training set (default is 0.8).
+
+    Returns:
+        tuple: A tuple containing two lists - the training set and the testing set.
+    """
+    if not 0 <= split_ratio <= 1:
+        raise ValueError("Split ratio must be between 0 and 1")
+
+    # Shuffle the data to ensure randomness in the split
+    random.shuffle(data)
+
+    # Calculate the split indices
+    split_index = int(len(data) * split_ratio)
+
+    # Split the data into training and testing sets
+    train_set = data[:split_index]
+    test_set = data[split_index:]
+
+    return train_set, test_set
+
+
+def subsample(data, ratio):
+    """
+    Subsample a list to a given ratio.
+
+    Args:
+        data (list): The list of items to be subsampled.
+        ratio (float): The ratio of items to retain in the subsample.
+
+    Returns:
+        list: A subsampled list containing the specified ratio of items.
+    """
+    if not 0 <= ratio <= 1:
+        raise ValueError("Ratio must be between 0 and 1")
+
+    # Calculate the number of items to retain in the subsample
+    num_items_to_retain = int(len(data) * ratio)
+
+    # Randomly select items to retain
+    subsampled_data = random.sample(data, num_items_to_retain)
+
+    return subsampled_data
+
+
+def write_jsonl(filename, data):
+    """
+    Write a list of dictionaries to a JSON Lines (JSONL) file.
+
+    Args:
+        filename (str): The name of the JSONL file to write to.
+        data (list): A list of dictionaries to write as JSONL objects.
+    """
+    with open(filename, 'w', encoding='utf-8') as file:
+        for item in data:
+            json.dump(item, file, ensure_ascii=False)
+            file.write('\n')
