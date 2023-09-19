47ffc5f4a5d1d0df269d007ae6792b48d3cb2842
Disiok <simonsdsuo@gmail.com>
2023-09-18 22:12:26 -0700

    update notebook with eval output and fix bug in embedding model exp

--- Ray-LlamaIndex/notebooks/utils.py
+++ Ray-LlamaIndex/notebooks/utils.py
@@ -1,48 +1,22 @@
-import numpy as np
 import json
 import random
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.embeddings.huggingface import HuggingFaceEmbeddings
-
-from llama_index.vector_stores import PGVectorStore
 from llama_index import VectorStoreIndex, ServiceContext
 from llama_index.llms import Anyscale, OpenAI
 
+from data import get_embedding_model, get_postgres_store, EMBEDDING_DIMENSIONS
 
-def get_embedding_model(model_name):
-    if model_name == "text-embedding-ada-002":
-            return OpenAIEmbeddings(
-                model=model_name,
-                openai_api_base=os.environ["OPENAI_API_BASE"],
-                openai_api_key=os.environ["OPENAI_API_KEY"])
-    else:
-        model_kwargs = {"device": "cuda"}
-        encode_kwargs = {"device": "cuda", "batch_size": 100}
-
-        return HuggingFaceEmbeddings(
-            model_name=model_name,
-            model_kwargs=model_kwargs,
-            encode_kwargs=encode_kwargs)
-    
-
-def get_postgres_store():
-    return PGVectorStore.from_params(
-            database="postgres", 
-            user="postgres", 
-            password="postgres", 
-            host="localhost", 
-            table_name="document",
-            port="5432",
-            embed_dim=768,
-        )
-    
     
 def _get_vector_store_index(
     service_context,
-    ):
+    embedding_model_name,
+):
 
-    vector_store = get_postgres_store()
-    index = VectorStoreIndex.from_vector_store(vector_store, service_context=service_context)
+    embed_dim = EMBEDDING_DIMENSIONS[embedding_model_name]
+    vector_store = get_postgres_store(embed_dim)
+    index = VectorStoreIndex.from_vector_store(
+        vector_store, 
+        service_context=service_context
+    )
     return index
 
 
@@ -61,7 +35,7 @@ def get_query_engine(
     
     service_context = ServiceContext.from_defaults(embed_model=embed_model, llm=llm)
 
-    index = _get_vector_store_index(service_context)
+    index = _get_vector_store_index(service_context, embedding_model_name)
     return index.as_query_engine(similarity_top_k=similarity_top_k)
 
 
@@ -73,7 +47,7 @@ def get_retriever(
     embed_model = get_embedding_model(embedding_model_name)
     service_context = ServiceContext.from_defaults(embed_model=embed_model, llm=None)
 
-    index = _get_vector_store_index(service_context)
+    index = _get_vector_store_index(service_context, embedding_model_name)
     return index.as_query_engine(similarity_top_k=similarity_top_k)
 
 
